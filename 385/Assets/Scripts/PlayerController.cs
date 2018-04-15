4a5d7a205a9a2896cebafdc4d5c81258cb0d10ab
Chris Johnston <chris@thejohnstons.net>
2018-04-14 20:44:23 -0700

    got the normal vector grapple swing normal working

--- 385/Assets/Scripts/PlayerController.cs
+++ 385/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+/// <summary>
+/// Controller script for the player
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// Toggle to show the debugging lines
+    /// </summary>
+    public bool ShowDebugging = true;
+
+    // constants for the inputs to check
+    // the strafe axis is defined negative going to the left and positive going
+    // to the right. Currently A goes left, D goes right
+    public const string STRAFE = "Strafe";
+
+    /// <summary>
+    /// Reference to the RopeSystem attribute of the Player.
+    /// If there are more than one, could make another one (or an array).
+    /// </summary>
+    public RopeSystem RopeSystem;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        // Check that the ref to RopeSystem is not null, and the rope system is connected
+        // the == true is not redundant because of the ?. operator
+        if (RopeSystem?.IsRopeConnected() == true)
+        {
+            // check the strafe axis
+            var strafeAmount = Input.GetAxis(STRAFE);
+
+            // get the perpendicular axis to the rope from the player
+            var perpendicularAxis = RopeSystem.GetRopePerpendicularAxis().Value;
+
+            // show some debug lines if debugging enabled
+            if (ShowDebugging)
+            {
+                Debug.DrawRay(transform.position, strafeAmount * perpendicularAxis, Color.red);
+            }
+        }
 	}
 }
--- 385/Assets/Scripts/RopeSystem.cs
+++ 385/Assets/Scripts/RopeSystem.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 /// <summary>
 /// Script for controlling the behavior of a rope, which is connected to the player
 /// and directly affects the physics that affect the player.
+/// 
+/// The ExecuteInEditModeAttribute allows the script update to run in the unity editor
+/// so that the previews are accurate.
 /// </summary>
+[ExecuteInEditMode]
 public class RopeSystem : MonoBehaviour
 {
     /// <summary>
@@ -25,13 +29,85 @@ public class RopeSystem : MonoBehaviour
     public LineRenderer RopeLineRenderer;
 
     /// <summary>
+    /// How much to offset where to draw the starting point of the player's
+    /// end of the rope LineRenderable? This should end up looking like it's in
+    /// the players hand, instead of emerging from their torso
+    /// 
+    /// The Z component should probably be 0
+    /// </summary>
+    public Vector3 PlayerRopeDrawOffset;
+
+    /// <summary>
     /// Util method to determine if this rope system is connected to a Rigidbody2D
     /// </summary>
     /// <returns>True, if the anchor point of the rope is connected, or false if not.</returns>
     public bool IsRopeConnected()
     {
-        return RopeAnchorPoint == null;
+        return RopeAnchorPoint != null;
     }
 
+    /// <summary>
+    /// Updates all of the positions of the rope LineRenderable
+    /// so that it is drawn accurately
+    /// </summary>
+    private void UpdateRopeRenderablePositions()
+    {
+        if (IsRopeConnected())
+        {
+            // ensure that the line renderer is enabled when the rope is connected
+            RopeLineRenderer.enabled = true;
+
+            // update the first point to be the same as the player origin w/ the offset
+            //TODO: should later expand on the player offset to compensate for any rotation of the player, if that is planned to be used
+            // could have the player rotate to match the swinging
+            RopeLineRenderer.SetPosition(0, transform.position + PlayerRopeDrawOffset);
+
+            // set the last point to be the same as the anchor point
+            RopeLineRenderer.SetPosition(1, RopeAnchorPoint.position);
+        }
+        else
+        {
+            // when the rope isn't connected, just disable the renderer
+            RopeLineRenderer.enabled = false;
+        }        
+    }
+
+    /// <summary>
+    /// Get the axis that is perpendicular to the rope from the player. This should default to pointing to the right when the player
+    /// hangs directly downward from the point they are hanging from.
+    /// </summary>
+    /// <returns>A Vector3 that represents the direction perpendicular to the rope, or null if not connected.</returns>
+    public Vector2? GetRopePerpendicularAxis()
+    {
+        // return null if the rope is not connected
+        if (!IsRopeConnected()) return null;
+
+        // get the vector value from the player position to the anchor point
+        var playerToAnchor = new Vector2(transform.position.x, transform.position.y) - RopeAnchorPoint.position;
+
+        return GetVector2Normal(playerToAnchor);
+    }
+
+    /// <summary>
+    /// Get the perpendicular value for a Vector2
+    /// </summary>
+    /// <returns></returns>
+    private Vector2 GetVector2Normal(Vector2 vec)
+    {
+        // get the perpendicular value
+        var val = new Vector2(-vec.y, vec.x);
+        // then normalize it
+        val.Normalize();
+        return val;
+    }
+    
+    /// <summary>
+    /// Update method
+    /// </summary>
+    private void Update()
+    {
+        // update the rope renderable positions
+        UpdateRopeRenderablePositions();
+    }
 
 }
