23eab3cac65ce7e7732016aef697e13a95952ac0
Chris Johnston <chris@thejohnstons.net>
2018-04-14 23:21:06 -0700

    another revision to build script

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -9,14 +9,7 @@
 
 ## Run the editor unit tests
 echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-runTests \
-	-editorTestsResultFile $(pwd)/test.xml \
-	-quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -logFile $(pwd)/unity.log -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}"	-runTests -editorTestsResultFile $(pwd)/test.xml -quit
 
 rc0=$?
 echo "Unit test logs"
@@ -27,28 +20,14 @@ if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 ## Make the builds
 # Recall from install.sh that a separate module was needed for Windows build support
 echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
-	-quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics	-silent-crashes	-logFile $(pwd)/unity.log	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}"	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" -quit
 
 rc1=$?
 echo "Build logs (Windows)"
 cat $(pwd)/unity.log
 
 echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
-	-quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode	-nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" -buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" -quit
 
 rc2=$?
 echo "Build logs (OSX)"
