34308b88359437e9d3bd61c97b2e6b2085115250
Saam Amiri <saam.amiri@gmail.com>
2018-04-20 01:55:25 -0700

    Added #6 kill trigger and menu integration

---
+++ 385/Assets/KillTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer>().color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	// When 
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Player had died");
+			Destroy (other.gameObject);
+		}
+	}
+
+
+
+}
