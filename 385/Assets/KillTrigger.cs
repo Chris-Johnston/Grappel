a7011026c31467dc122518e8c7e8a52cbcee4b28
Saam Amiri <saam.amiri@gmail.com>
2018-04-20 01:59:21 -0700

    Fixed file formating

--- 385/Assets/KillTrigger.cs
+++ 385/Assets/KillTrigger.cs
@@ -6,21 +6,20 @@ public class KillTrigger : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<SpriteRenderer>().color = Color.red;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	// When 
+
+	// Adds kill trigger to player 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			Debug.Log ("Player had died");
 			Destroy (other.gameObject);
 		}
 	}
-
-
 
 }
