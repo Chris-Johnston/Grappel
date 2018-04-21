0f22fa4b72b8a76afa068336f3e0a8f693672128
Chris Johnston <chris@thejohnstons.net>
2018-04-20 20:00:01 -0700

    misc changes before prototype 1 release

---
+++ 385/Assets/Scripts/KillTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Adds kill trigger to player 
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Player had died");
+			// Destroy (other.gameObject);
+		}
+	}
+
+}
