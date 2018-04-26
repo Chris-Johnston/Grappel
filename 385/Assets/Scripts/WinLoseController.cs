44f88ba4f28480f6eee41e6c47b601b310d0a8a5
Chris Johnston <chris@thejohnstons.net>
2018-04-25 20:18:18 -0700

    Refactor the way that game state triggers work (#56)
    Make triggers generic and just call a list of methods when the player interacts with them
    Adds some placeholder objects and sprites
    adjusts the opactiy of the red sprite

---
+++ 385/Assets/Scripts/WinLoseController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLoseController : MonoBehaviour
+{
+    /// <summary>
+    /// Object to enable when the player wins
+    /// </summary>
+    public GameObject WinObject;
+
+    /// <summary>
+    /// Object to enable when the player loses
+    /// </summary>
+    public GameObject LoseObject;
+
+	void Start ()
+    {
+        // disable both objects by default
+        WinObject?.SetActive(false);
+        LoseObject?.SetActive(false);
+	}
+	
+    /// <summary>
+    /// Enable the win object, disable the lose object
+    /// </summary>
+	public void Win()
+    {
+        WinObject?.SetActive(true);
+        LoseObject?.SetActive(false);
+    }
+
+    /// <summary>
+    /// Enable the lose object, disable the win object
+    /// </summary>
+    public void Lose()
+    {
+        WinObject?.SetActive(false);
+        LoseObject?.SetActive(true);
+    }
+}
