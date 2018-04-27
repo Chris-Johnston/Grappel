7f5c9792c3130ff7e3a1861486fbd4a96e5de432
enbaik <37854417+enbaik@users.noreply.github.com>
2018-04-27 00:42:05 -0700

    Smoother Camera Movement
    Camera transitions to and from player position and initial position in smoother motion.

--- 385/Assets/Scripts/PlayerCamera.cs
+++ 385/Assets/Scripts/PlayerCamera.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using Assets.Scripts;
 
 public class PlayerCamera : MonoBehaviour {
@@ -9,6 +10,8 @@ public class PlayerCamera : MonoBehaviour {
 
     // offset of camera from player
     private Vector3 offset;
+
+    private Vector3 initPos;
 
     // camera that follows player
     public Camera playerCam;
@@ -19,10 +22,19 @@ public class PlayerCamera : MonoBehaviour {
     // used to check is camera button is pushed
     private AxisButton CameraButton;
 
+    public bool trackPlayer = false;
+
+    public float moveSpeed = 1;
+
+    public float distance = 2, X;
+
     // gets offset for camera and starts camera in static view
     void Start () {
-        offset = transform.position - player.transform.position;
-        playerCam.enabled = false;
+        float Y = distance / 2;
+        Vector3 temp = new Vector3(-X, Y, distance);
+        offset = transform.position - player.transform.position + temp;
+
+        initPos = transform.position;
 
         // camera axis button created
         CameraButton = new AxisButton(CameraAxis, 0.5f);
@@ -31,12 +43,24 @@ public class PlayerCamera : MonoBehaviour {
     // updates camera button and camera position
 	void LateUpdate () {
         CameraButton.Update();
-        transform.position = player.transform.position + offset;
 
         // checks if camera button has been pressed
         if (CameraButton.IsButtonClicked())
         {
-            playerCam.enabled = !playerCam.enabled;
+            trackPlayer = !trackPlayer;
+        }
+
+        if (trackPlayer)
+        {
+            float step = moveSpeed * Time.deltaTime;
+            Vector3 temp = player.transform.position + offset;
+            transform.position = Vector3.MoveTowards(transform.position,temp, step);
+
+        }
+        else
+        {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, initPos, step);
         }
     }
 
