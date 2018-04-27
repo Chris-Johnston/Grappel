5be7e9277f7471b4f026c10fca7449b769937395
enbaik <37854417+enbaik@users.noreply.github.com>
2018-04-27 16:57:52 -0700

    Camera tracks rope connection
    Camera now follows player in smoother motion and lock on to swinging point when connected

--- 385/Assets/Scripts/PlayerCamera.cs
+++ 385/Assets/Scripts/PlayerCamera.cs
@@ -11,6 +11,7 @@ public class PlayerCamera : MonoBehaviour {
     // offset of camera from player
     private Vector3 offset;
 
+    // Static position of camera
     private Vector3 initPos;
 
     // camera that follows player
@@ -26,15 +27,20 @@ public class PlayerCamera : MonoBehaviour {
 
     public float moveSpeed = 1;
 
-    public float distance = 2, X;
+    public float distance = 2, X; // controls distance of camera and 
+                                  // its X axis adjustment
+
+    private bool camMoving = false;
+
+    public RopeSystem RopeSystem;
 
     // gets offset for camera and starts camera in static view
     void Start () {
-        float Y = distance / 2;
-        Vector3 temp = new Vector3(-X, Y, distance);
+        float Y = distance / 2; // Y axis adjustment for changing the camera distance
+        Vector3 temp = new Vector3(-X, Y, distance);  // stores adjustments
         offset = transform.position - player.transform.position + temp;
 
-        initPos = transform.position;
+        initPos = transform.position;  // stores static camera position
 
         // camera axis button created
         CameraButton = new AxisButton(CameraAxis, 0.5f);
@@ -45,23 +51,82 @@ public class PlayerCamera : MonoBehaviour {
         CameraButton.Update();
 
         // checks if camera button has been pressed
-        if (CameraButton.IsButtonClicked())
-        {
+        if (CameraButton.IsButtonClicked()) {
+            camMoving = true;
             trackPlayer = !trackPlayer;
         }
 
-        if (trackPlayer)
+        if(camMoving)  // if camera is still adjusting
         {
+            // adjust move speed
             float step = moveSpeed * Time.deltaTime;
-            Vector3 temp = player.transform.position + offset;
-            transform.position = Vector3.MoveTowards(transform.position,temp, step);
+
+            
+            if (trackPlayer){ // moving towards player
+
+                if (RopeSystem.IsRopeConnected())
+                {   // moving towards swing point
+                    Vector3 camTemp = new Vector3(0, 0, -12);
+                    Rigidbody2D ropeConnect = RopeSystem.getAnchor();
+                    if (playerCam.transform.position !=
+                        ropeConnect.transform.position + camTemp)
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position,
+                            ropeConnect.transform.position + camTemp, step);
+                    }
+                    else  // reached position
+                    {
+                        camMoving = false;
+                    }
+
+                }
+                else  // moving towards player position
+                {
+                    // uses moveTowards to move camera
+                    Vector3 temp = player.transform.position + offset;
+                    if (playerCam.transform.position != temp)
+                    {
+                        transform.position = Vector3.MoveTowards(
+                        transform.position, temp, step);
+                    }
+                    else  // reached position
+                    {
+                        camMoving = false;
+                    }
+                }
+                
+                    
+             }
+            else{   // moves towards init static position
+                  if(playerCam.transform.position != initPos){
+                    transform.position = Vector3.MoveTowards(
+                        transform.position, initPos, step);
+                   }
+                else  // reached position
+                {
+                    camMoving = false;
+                }
+                    
+            }
+            
+        }
+
+        if (trackPlayer && !camMoving)
+        {
+            // adjust move speed
+            float step = moveSpeed * Time.deltaTime;
+
+            if (RopeSystem.IsRopeConnected())
+            {
+                camMoving = true;
+            }
+            else
+            {
+                transform.position = player.transform.position + offset;
+            }
 
         }
-        else
-        {
-            float step = moveSpeed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, initPos, step);
-        }
+
     }
 
 }
