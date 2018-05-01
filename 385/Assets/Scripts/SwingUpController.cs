ffc8d31606dd5e771074317e8b866140349218e7
Chris Johnston <chris@thejohnstons.net>
2018-05-01 02:26:40 -0700

    Add more revisions and additions for polish

--- 385/Assets/Scripts/SwingUpController.cs
+++ 385/Assets/Scripts/SwingUpController.cs
@@ -1,9 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SwingUpController : MonoBehaviour
 {
+    /// <summary>
+    /// At what point should the water cheat to catch up to the player
+    /// </summary>
+    public const float WATER_CATCHUP_DISTANCE = 11.5f;
+    /// <summary>
+    /// Max number of hook isntances to have spawned all at once
+    /// </summary>
+    public const int MAX_NUM_INSTANCES = 25;
+
+    /// <summary>
+    /// Set to true when the player has lost
+    /// </summary>
+    public bool LoseState = false;
+
+    /// <summary>
+    /// the text that shows how high the player has gone
+    /// in the format
+    /// 
+    /// Height: 123.0
+    /// </summary>
+    public Text HeightText;
+
+    /// <summary>
+    /// Objects that are enabled on lose
+    /// </summary>
+    public GameObject LoseObjects;
+
+    /// <summary>
+    /// The text that reads the final height score
+    /// </summary>
+    public Text FinalHeightText;
+
+    /// <summary>
+    /// The maxium height reached by the player
+    /// </summary>
+    public int MaxHeight = 0;
 
     /// <summary>
     /// Reference to the camera to use
@@ -31,12 +69,19 @@ public class SwingUpController : MonoBehaviour
     private Vector3 CameraGrabPos;
 
     /// <summary>
+    /// The target position of the water level
+    /// </summary>
+    private Vector3 WaterTargetPosition;
+
+    /// <summary>
     /// The position of the lower left bound to spawn objects
+    /// relative to the current CameraGrabPosition
     /// </summary>
     public Vector3 SpawnPositionBottomLeftBound;
 
     /// <summary>
-    /// The position o the upper right bound to spawn objects
+    /// The position of the upper right bound to spawn objects
+    /// relative to the current CameraGrabPosition
     /// </summary>
     public Vector3 SpawnPositionUpperRightBound;
 
@@ -66,8 +111,22 @@ public class SwingUpController : MonoBehaviour
     public void PlayerDie()
     {
         Debug.Log("Ded");
+
+        FinalHeightText.text = $"Final Height: {MaxHeight}";
+        LoseObjects.SetActive(true);
+
+        // disable and clear all the points
+        foreach (var point in PointInstances)
+        {
+            point.SetActive(false);
+        }
+
+        PointInstances.Clear();
+
         // enable the lose text, show the score
         // wait a few seconds and then reload the scene
+
+        StartCoroutine(RestartCurrentLevelAfterTime(3.0f));
     }
 
 	void Start ()
@@ -77,6 +136,8 @@ public class SwingUpController : MonoBehaviour
 
         // set the initial position for the camera
         CameraGrabPos = ActiveMainCamera.transform.position;
+
+        WaterTargetPosition = WaterReference.transform.position;
 	}
 
     /// <summary>
@@ -86,7 +147,9 @@ public class SwingUpController : MonoBehaviour
     {
         // spawn a new grapple point in the region between the two vector3s
         var range = SpawnPositionUpperRightBound - SpawnPositionBottomLeftBound;
-        var point = SpawnPositionBottomLeftBound + new Vector3(range.x * Random.value, range.y * Random.value, range.z * Random.value);
+        var point = CameraGrabPos + SpawnPositionBottomLeftBound + new Vector3(range.x * Random.value, range.y * Random.value, range.z * Random.value);
+        // enforce the Z to be at 0, ignore the camera position
+        point.z = 0f;
 
         var newItem = Instantiate(GrapplePointPrefab);
         // should set the parent of this prefab to be this object
@@ -117,30 +180,80 @@ public class SwingUpController : MonoBehaviour
                 // then update the y position of the grab point
                 CameraGrabPos = new Vector3(0, position.y, -10);
 
-                
+                // spawn three points when this happens
+                SpawnNewGrapplePoint();
+                SpawnNewGrapplePoint();
+                SpawnNewGrapplePoint();
 
-                // spawn two points when this happens
-                SpawnNewGrapplePoint();
-                SpawnNewGrapplePoint();
+                //Debug.Log($"Difference was {CameraGrabPos.y - WaterReference.transform.position.y}");
+
+                // if the water is behind, then let it catch up
+                if (CameraGrabPos.y - WaterReference.transform.position.y > WATER_CATCHUP_DISTANCE)
+                {
+                    //Debug.Log("Update");
+                    var newPos = (CameraGrabPos - new Vector3(0, WATER_CATCHUP_DISTANCE, 0));
+                    newPos.z = 0;   
+                    WaterTargetPosition = newPos;
+                }
             }
         }
         else
         {
-            // check that the player isn't too far above
-            if (CameraGrabPos.y < PlayerReference.transform.position.y + 4)
+            // check that the player isn't going out of bounds of the camera
+            if (CameraGrabPos.y < PlayerReference.transform.position.y - 3)
             {
-                // Debug.Log("The player went too high");
+                // Debug.Log("Player went high");
+
+                CameraGrabPos += new Vector3(0, 4, 0);
+
+                // spawn three points when this happens
+                SpawnNewGrapplePoint();
+                SpawnNewGrapplePoint();
+                SpawnNewGrapplePoint();
             }
         }
 
         // update the position of the camera
         if (CameraGrabPos != null)
         {
-            ActiveMainCamera.transform.position = CameraGrabPos;
+
+            ActiveMainCamera.transform.position = Vector3.MoveTowards(ActiveMainCamera.transform.position, CameraGrabPos, 0.3f);
+            //ActiveMainCamera.transform.position = CameraGrabPos;
         }
 
         // update the positioon of the water
-        WaterReference.transform.position += WaterMoveRatePerSecond * Time.deltaTime;
+        WaterTargetPosition += (1f + MaxHeight / 70.0f) * WaterMoveRatePerSecond * Time.deltaTime;
+
+        Debug.Log($"Current rate : {(1f + MaxHeight / 30.0f) * WaterMoveRatePerSecond}");
+
+        if (WaterTargetPosition != null && !LoseState)
+        {
+            WaterReference.transform.position = Vector3.MoveTowards(WaterReference.transform.position, WaterTargetPosition, 0.2f);
+        }            
+
+        if (HeightText != null)
+        {
+            if (PlayerReference.transform.position.y > MaxHeight)
+            {
+                MaxHeight = (int)PlayerReference.transform.position.y;
+            }
             
+            HeightText.text = $"Height: {MaxHeight}";
+        }
+
+        if (PointInstances?.Count > MAX_NUM_INSTANCES)
+            PointInstances.RemoveRange(0, PointInstances.Count - MAX_NUM_INSTANCES);
 	}
+
+    /// <summary>
+    /// Waits the specified duration then reloads the level
+    /// </summary>
+    /// <param name="delay">time to wait in seconds</param>
+    /// <returns></returns>
+    IEnumerator RestartCurrentLevelAfterTime(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
---
+++ 385/Assets/Scripts/WaterWarnController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// basically the same as AreaTriggerCollider
+/// but has behavior on trigger exit
+/// </summary>
+public class WaterWarnController : MonoBehaviour
+{
+    public const string CollideTag = Tags.TAG_PLAYER;
+
+    // ref to this object's renderer
+    private MeshRenderer meshRenderer;
+
+	void Start ()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(CollideTag))
+        {
+            meshRenderer.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(CollideTag))
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
