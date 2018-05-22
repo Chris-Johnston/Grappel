7b8ebe3116310a8ef5dcfdcd76b32a8103d98d3b
Chris Johnston <chris@thejohnstons.net>
2018-05-21 19:01:49 -0700

    refactor the kill trigger behavior (are we still using this?)

--- 385/Assets/Scripts/KillTrigger.cs
+++ 385/Assets/Scripts/KillTrigger.cs
@@ -2,24 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillTrigger : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	// Adds kill trigger to player 
-	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.tag == "Player") {
+public class KillTrigger : MonoBehaviour
+{
+	/// <summary>
+    /// Destroys the game objects with the tag "Player"
+    /// when they collide with something that uses this behavior
+    /// </summary>
+    /// <param name="other"></param>
+	void OnTriggerEnter2D (Collider2D other)
+    {
+		if (other.gameObject.tag == Tags.TAG_PLAYER) {
 			Debug.Log ("Player had died");
 		    Destroy (other.gameObject);
 		}
 	}
-
 }
