a92649a264767c82d7d7dc4fec8e39404c8ba73b
Chris Johnston <chris@thejohnstons.net>
2018-05-31 04:49:50 -0700

    Add a less awful title screen (#111)
    * Replace the awful mspaint background art with some nicer ones used google fonts - acme
    
    * Add some 3d effect to the main menu to make it less boring
    
    * rename challenge mode -> endurance mode
    
    * Small visual tweaks to the swing up scene so that it matches the rest of the game

---
+++ 385/Assets/Scripts/MainMenuCameraWiggler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Wiggles the camera around on the main menu to make it look cool or something
+/// wiggle the x position of the camera along a sine wave and have the camera always point towards the origin
+/// </summary>
+public class MainMenuCameraWiggler : MonoBehaviour
+{
+    /// <summary>
+    /// Where the camera should always be facing
+    /// </summary>
+    public Vector3 AlwaysPointPosition = Vector3.zero;
+
+    /// <summary>
+    /// How strong the camera wiggling action should be
+    /// </summary>
+    [Range(0, 10)]
+    public float Ampltude = 5.0f;
+
+    /// <summary>
+    /// how quick the camera wiggling action is
+    /// </summary>
+    [Range(0.1f, 5)]
+    public float Frequency = 0.3f;
+
+    /// <summary>
+    /// the original position of the camera
+    /// </summary>
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        // store the original position so that we can use it for wigglin'
+        originalPosition = transform.position;
+    }
+
+	void Update ()
+    {
+        // wiggle the x axis of the camera
+        var pos = originalPosition;
+        pos.x += Mathf.Sin(Time.time / Frequency) * Ampltude;
+        transform.position = pos;
+
+        // update the camera to always point towards the position specified
+        transform.LookAt(AlwaysPointPosition);
+	}
+}
